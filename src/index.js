2c35aacac825c471186945fc0c6c2e8051c505d0
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-03-04 22:11:01 +0300

    init: expression-calculator task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  let stack = [];
+  let last;
+  let finalResult;
+  ("(" + expr + ")").split("").forEach((symbol, index) => {
+    if (symbol == "(") {
+      last = { str: "" };
+      stack.push(last);
+    } else if (symbol == ")") {
+      let result = calculate(stack.pop().str);
+
+      if (stack.length == 0) {
+        if (expr.length + 1 == index){
+            finalResult = result;
+        } 
+        else throw "ExpressionError: Brackets must be paired";
+      }
+      else{
+        last = stack[stack.length - 1];
+        last.str += result;
+      }
+    } else last.str += symbol;
+    console.log(stack);
+  });
+  if(!finalResult) throw "ExpressionError: Brackets must be paired";
+  return finalResult;
+}
+
+console.log(expressionCalculator("(2 + 3 + (7 + 5))"));
+console.log(expressionCalculator("2 + 3 + (7 + 5)"));
+// console.log(expressionCalculator("2 + 3 + (7 + 5"));
+console.log(expressionCalculator("2 + 3 + 7 + 5)"));
+
+function calculate(str) {
+  return str;
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator
+};
